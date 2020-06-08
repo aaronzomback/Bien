1cde02ea873a6f5ce1a2f91d94f7e9bf110d845c
aaronzomback <aaronzomback@gmail.com>
2020-06-08 16:08:24 +0200

    added styles and some validations

---
+++ syntax.rb
@@ -0,0 +1,29 @@
+
+# variables
+@number = 13
+@number = 14
+
+
+# variables w/ strings (can change and combine - Aaron Zomback + admin)
+
+@username = "Aaron Zomback" + "admin"
+
+
+# symbols (can't be changed)
+
+@method = :delete
+
+
+# lists
+
+@shopping_list = ["bacon", "eggs", "sausages"]
+
+
+# Hash   (a list but describes one thing instead)
+
+@person = {first_name: "aaron", age: 29, shopping: ["eggs"]}
+
+
+# Has with a key equal to a symbol
+
+@link = { method: :delete }
